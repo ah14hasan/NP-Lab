3a3386ce37429806ba763d1996ddeb492b0e8756
Ali Hasan <ah14hasan@gmail.com>
2025-11-18 11:40:45 +0530

    Implement sender program using named pipes

---
+++ Named Pipes/send.c
@@ -0,0 +1,28 @@
+// IPC using named pipes (sender's program)
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#define FIFO_PATH "/tmp/myfifo"
+
+int main() {
+int fd;
+char arr[100];
+
+ // Create the named pipe (FIFO) if it does not exist
+mkfifo(FIFO_PATH, 0666);
+
+printf("Enter a message to send: ");
+fgets(arr, sizeof(arr), stdin);
+
+ // Open FIFO for writing
+fd = open(FIFO_PATH, O_WRONLY);
+write(fd, arr, strlen(arr)+1);
+close(fd);
+
+printf("Message sent.\n");
+return 0;
+}
