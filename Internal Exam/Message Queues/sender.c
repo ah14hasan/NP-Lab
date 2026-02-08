137f6788905bd1c6ad80542c4cbd9604902563f1
Ali Hasan <ah14hasan@gmail.com>
2026-02-08 11:54:18 +0530

    Update sender.c

--- Internal Exam/Message Queues/sender.c
+++ Internal Exam/Message Queues/sender.c
@@ -1,4 +1,4 @@
-// sender.c : Create a message queue and send 3 messages with different priorities
+// sender.c : Create a message queue and send 3 messages with user-given priorities and texts
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,56 +7,59 @@
 
 #define MAX_TEXT 128
 
-// User-defined message structure: first field MUST be long (message type/priority)
 struct msgbuf {
-    long mtype;              // Used here as priority (must be > 0)
-    char mtext[MAX_TEXT];    // Message text
+    long mtype;              // message type/priority (> 0)
+    char mtext[MAX_TEXT];    // message text
 };
 
 int main(void) {
     key_t key;
     int msqid;
     struct msgbuf message;
+    int i;
 
-    // Generate a key for the message queue
     key = ftok(".", 'A');
     if (key == -1) {
         perror("ftok");
         exit(EXIT_FAILURE);
     }
 
-    // Create a message queue with read & write permissions: 0666
     msqid = msgget(key, IPC_CREAT | 0666);
     if (msqid == -1) {
         perror("msgget");
         exit(EXIT_FAILURE);
     }
 
-    // --------- First message (priority 1) ----------
-    message.mtype = 1;
-    strcpy(message.mtext, "First message with priority 1");
-    printf("About to send: \"%s\" with type %ld\n", message.mtext, message.mtype);
-    if (msgsnd(msqid, &message, strlen(message.mtext) + 1, 0) == -1) {
-        perror("msgsnd - 1");
-        exit(EXIT_FAILURE);
-    }
+    for (i = 0; i < 3; i++) {
+        printf("Enter priority for message %d (positive integer): ", i + 1);
+        if (scanf("%ld", &message.mtype) != 1 || message.mtype <= 0) {
+            fprintf(stderr, "Invalid priority.\n");
+            exit(EXIT_FAILURE);
+        }
 
-    // --------- Second message (priority 2) ----------
-    message.mtype = 2;
-    strcpy(message.mtext, "Second message with priority 2");
-    printf("About to send: \"%s\" with type %ld\n", message.mtext, message.mtype);
-    if (msgsnd(msqid, &message, strlen(message.mtext) + 1, 0) == -1) {
-        perror("msgsnd - 2");
-        exit(EXIT_FAILURE);
-    }
+        // Clear leftover newline from input buffer before using fgets.
+        int ch;
+        while ((ch = getchar()) != '\n' && ch != EOF)
+            ;
 
-    // --------- Third message (priority 3) ----------
-    message.mtype = 3;
-    strcpy(message.mtext, "Third message with priority 3");
-    printf("About to send: \"%s\" with type %ld\n", message.mtext, message.mtype);
-    if (msgsnd(msqid, &message, strlen(message.mtext) + 1, 0) == -1) {
-        perror("msgsnd - 3");
-        exit(EXIT_FAILURE);
+        printf("Enter text for message %d: ", i + 1);
+        if (fgets(message.mtext, MAX_TEXT, stdin) == NULL) {
+            fprintf(stderr, "Error reading message text.\n");
+            exit(EXIT_FAILURE);
+        }
+
+        // Remove trailing newline from fgets, if present.
+        size_t len = strlen(message.mtext);
+        if (len > 0 && message.mtext[len - 1] == '\n') {
+            message.mtext[len - 1] = '\0';
+        }
+
+        printf("About to send: \"%s\" with type %ld\n", message.mtext, message.mtype);
+
+        if (msgsnd(msqid, &message, strlen(message.mtext) + 1, 0) == -1) {
+            perror("msgsnd");
+            exit(EXIT_FAILURE);
+        }
     }
 
     printf("Three messages sent to the message queue.\n");
